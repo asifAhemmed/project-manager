c242ff568c333b68a17304cedf133e512ffc2e9b
asif ahemmed <asifahemmed0@gmail.com>
2024-11-23 22:39:09 +0600

    project completed

--- src/projectBoard/CardSort.jsx
+++ src/projectBoard/CardSort.jsx
@@ -1,24 +1,40 @@
+import { useState, useContext } from "react";
+import { TaskContext } from "../contexts";
+import { GoSortAsc, GoSortDesc } from "react-icons/go";
 const CardSort = () => {
+  const { data, setData } = useContext(TaskContext);
+  const [sortingOrder, setSortingOrder] = useState("asc");
+  const handleSort = () => {
+    if (sortingOrder === "asc") {
+      setData({
+        ...data,
+        toDo: data.toDo.sort((a, b) => new Date(a.date) - new Date(b.date)),
+        onProgress: data.onProgress.sort(
+          (a, b) => new Date(a.date) - new Date(b.date)
+        ),
+        done: data.done.sort((a, b) => new Date(a.date) - new Date(b.date)),
+        revise: data.revise.sort((a, b) => new Date(a.date) - new Date(b.date)),
+      });
+      setSortingOrder("desc");
+    } else {
+      setData({
+        ...data,
+        toDo: data.toDo.sort((a, b) => new Date(b.date) - new Date(a.date)),
+        onProgress: data.onProgress.sort(
+          (a, b) => new Date(b.date) - new Date(a.date)
+        ),
+        done: data.done.sort((a, b) => new Date(b.date) - new Date(a.date)),
+        revise: data.revise.sort((a, b) => new Date(b.date) - new Date(a.date)),
+      });
+      setSortingOrder("asc");
+    }
+  };
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-sort-descending"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M4 6l9 0" />
-      <path d="M4 12l7 0" />
-      <path d="M4 18l7 0" />
-      <path d="M15 15l3 3l3 -3" />
-      <path d="M18 6l0 12" />
-    </svg>
+    <>
+      <button onClick={handleSort} className="text-2xl">
+        {sortingOrder === "asc" ? <GoSortAsc /> : <GoSortDesc />}
+      </button>
+    </>
   );
 };
 
--- src/projectBoard/ProjectCard.jsx
+++ src/projectBoard/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({ onEditTask,onDeleteTask,data,name,color }) => {
     <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
       <div className={`rounded-lg ${color} p-4`}>
         <div className="mb-2 flex items-center justify-between">
-          <h3 className="text-lg font-semibold">{name}</h3>
+          <h3 className="text-lg font-semibold">{name} ({data.length})</h3>
           <CardSort />
         </div>
         <div>
